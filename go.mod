85b295bfabdffc337a4c512c578d8cffd309fdc8
Eyob <eabebe91@gmail.com>
2025-02-19 13:58:29 +0300

    feat: route setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eyoba-bisru/go_auth
 
 go 1.21.6
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	println("Hello")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	authRoute := chi.NewRouter()
+	authRoute.Get("/signup", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Man"))
+	})
+
+	r.Mount("/auth", authRoute)
+
+	http.ListenAndServe(":8080", r)
 }
