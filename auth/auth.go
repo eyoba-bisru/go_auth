94fc472b9807993ec9a2c4e8208d3afc6a86d08d
Eyob <eabebe91@gmail.com>
2025-02-28 12:50:05 +0300

    signin route added

--- auth/auth.go
+++ auth/auth.go
@@ -30,6 +30,7 @@ func Auth() *chi.Mux {
 		err := json.NewDecoder(r.Body).Decode(&parsedBody)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 
 		// Validate
@@ -38,15 +39,15 @@ func Auth() *chi.Mux {
 			return
 		}
 
-		database.First(&user, "email = ?", parsedBody.Email)
-		if user.Email != "" {
-			http.Error(w, "user already exit", http.StatusBadRequest)
-			return
-		}
-
 		validatedEmail, err := mail.ParseAddress(parsedBody.Email)
 		if err != nil {
 			http.Error(w, "invalid email", http.StatusBadRequest)
+			return
+		}
+
+		database.First(&user, "email = ?", parsedBody.Email)
+		if user.Email != "" {
+			http.Error(w, "user already exit", http.StatusBadRequest)
 			return
 		}
 
@@ -57,27 +58,26 @@ func Auth() *chi.Mux {
 			return
 		}
 
-		accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		secretKey := os.Getenv("SECRET_KEY")
+
+		accessTokenString, err := CreateJWT(jwt.MapClaims{
 			"exp":   time.Now().Add(time.Hour).Unix(),
-			"email": validatedEmail,
-			"hash":  hashedPassword,
-		})
+			"email": validatedEmail.Address,
+		}, secretKey, true, w)
 
-		secret_key := os.Getenv("SECRET_KEY")
-
-		accessTokenString, err := accessToken.SignedString([]byte(secret_key))
 		if err != nil {
 			http.Error(w, "Access Error", http.StatusInternalServerError)
+			return
 		}
 
-		refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		refreshTokenString, err := CreateJWT(jwt.MapClaims{
 			"exp":   time.Now().Add(time.Hour * 8760).Unix(),
 			"email": validatedEmail.Address,
-		})
+		}, secretKey, false, w)
 
-		refreshTokenString, err := refreshToken.SignedString([]byte(secret_key))
 		if err != nil {
 			http.Error(w, "Refresh Error", http.StatusInternalServerError)
+			return
 		}
 
 		http.SetCookie(w, &http.Cookie{
@@ -102,10 +102,89 @@ func Auth() *chi.Mux {
 
 		if err != nil {
 			http.Error(w, "Marshal Error", http.StatusInternalServerError)
+			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(res)
+	})
+
+	authRoute.Post("/signin", func(w http.ResponseWriter, r *http.Request) {
+		var parsedBody Body
+
+		secretKey := os.Getenv("SECRET_KEY")
+
+		err := json.NewDecoder(r.Body).Decode(&parsedBody)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Validate
+		if parsedBody.Email == "" || parsedBody.Password == "" {
+			http.Error(w, "email or password missing", http.StatusBadRequest)
+			return
+		}
+
+		validatedEmail, err := mail.ParseAddress(parsedBody.Email)
+		if err != nil {
+			http.Error(w, "invalid email", http.StatusBadRequest)
+			return
+		}
+
+		database.First(&user, "email = ?", parsedBody.Email)
+		if user.Email == "" {
+			http.Error(w, "user doen't exit", http.StatusBadRequest)
+			return
+		}
+
+		isVerified := VerifyPassword(parsedBody.Password, user.Password)
+		if !isVerified {
+			http.Error(w, "wrong email or password", http.StatusBadRequest)
+			return
+		}
+
+		accessTokenString, err := CreateJWT(jwt.MapClaims{
+			"exp":   time.Now().Add(time.Hour).Unix(),
+			"email": validatedEmail.Address,
+		}, secretKey, true, w)
+		if err != nil {
+			http.Error(w, "Access Error", http.StatusInternalServerError)
+			return
+		}
+
+		refreshTokenString, err := CreateJWT(jwt.MapClaims{
+			"exp":   time.Now().Add(time.Hour * 8760).Unix(),
+			"email": validatedEmail.Address,
+		}, secretKey, false, w)
+		if err != nil {
+			http.Error(w, "Refresh Error", http.StatusInternalServerError)
+			return
+		}
+
+		http.SetCookie(w, &http.Cookie{
+			Name:  "access",
+			Value: accessTokenString,
+		})
+		http.SetCookie(w, &http.Cookie{
+			Name:  "refresh",
+			Value: refreshTokenString,
+		})
+
+		var response = map[string]bool{
+			"success": true,
+		}
+
+		res, err := json.Marshal(&response)
+
+		if err != nil {
+			http.Error(w, "Marshal Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+
 	})
 
 	return authRoute
@@ -119,4 +198,21 @@ func HashPassword(password string) (string, error) {
 func VerifyPassword(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func CreateJWT(claims jwt.MapClaims, secretKey string, isAccess bool, w http.ResponseWriter) (string, error) {
+
+	if isAccess {
+		accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		accessTokenString, err := accessToken.SignedString([]byte(secretKey))
+
+		return accessTokenString, err
+	}
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	refreshTokenString, err := refreshToken.SignedString([]byte(secretKey))
+
+	return refreshTokenString, err
+
 }
